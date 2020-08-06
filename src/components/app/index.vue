1128deeb3aff0b3ca4aceac92b82ddacfb12ff45
Christopher Ribeiro <christo_campiglia@hotmail.com>
2020-08-06 06:03:26 -0300

    Add app component.

---
+++ src/components/app/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <slot/>
+  </div>
+</template>
+
+<script>
+require('@/styles/reset/main.scss');
+require('@/styles/typography/main.scss');
+
+export default {
+  name: 'App',
+};
+</script>
