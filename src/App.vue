9e3a3db07b592d04d6e8a352e8b24a4b5c730bfa
Christopher Ribeiro <christo_campiglia@hotmail.com>
2020-09-25 11:24:46 -0300

    Update test bed.

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,26 @@
     <h2>Header 2</h2>
     <h3>Header 3</h3>
     <h4>Header 4</h4>
-    <h5>Header 5</h5>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit.
       Iusto autem, sequi magnam animi hic voluptas qui adipisci suscipit,
       numquam distinctio explicabo quae, molestiae repellat! Velit,
       dignissimos doloribus? Libero, laborum saepe!
     </p>
-    <SButton text="yo"/>
-    <SButton text="yo" outlined/>
-    <SButton text="yo" disabled/>
-    <SButton text="yo" outlined disabled/>
+    <SButton text="Primary" block />
+    <SButton text="Primary" block outlined/>
+    <SButton text="Primary" block disabled/>
+    <SButton text="Primary" block outlined disabled/>
 
-    <SButton text="yo" primary/>
-    <SButton text="yo" outlined primary/>
-    <SButton text="yo" primary disabled/>
-    <SButton text="yo" outlined primary disabled/>
+    <SButton text="Primary" block primary/>
+    <SButton text="Primary" block primary outlined/>
+    <SButton text="Primary" block primary disabled/>
+    <SButton text="Primary" block primary outlined disabled/>
 
-    <SButton text="yo" danger/>
-    <SButton text="yo" outlined danger/>
-    <SButton text="yo" danger disabled/>
-    <SButton text="yo" outlined danger disabled/>
+    <SButton text="Primary" block danger/>
+    <SButton text="Primary" block danger outlined/>
+    <SButton text="Primary" block danger disabled/>
+    <SButton text="Primary" block danger outlined disabled/>
   </S-App>
 </template>
 
