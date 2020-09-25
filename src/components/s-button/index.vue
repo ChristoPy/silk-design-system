27a6480ad5931a046819ceae29f4bb3b1bba9f85
Christopher Ribeiro <christo_campiglia@hotmail.com>
2020-09-25 11:24:22 -0300

    Update button component.
    Add vue component.
    Add active state.
    Add block type.
    Fix disabled state.
    Update outlined button to use inner shadow instead of a border.

---
+++ src/components/s-button/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <button :class="{ ...type, outlined, block }">{{ text }}</button>
+</template>
+
+<script>
+require('../../styles/button/main.scss');
+
+export default {
+  name: 'S-Button',
+  props: {
+    text: {
+      type: String,
+      required: false,
+    },
+    primary: {
+      type: Boolean,
+      required: false,
+    },
+    danger: {
+      type: Boolean,
+      required: false,
+    },
+    gray: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    outlined: {
+      type: Boolean,
+      required: false,
+    },
+    block: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  computed: {
+    type() {
+      const { primary, danger, gray } = this;
+
+      if (primary || danger) {
+        return { primary, danger };
+      }
+      return { gray };
+    },
+  },
+};
+</script>
