6756c3cf87bff13dcb97a268ae71f2a2d22d0a9b
Christopher Ribeiro <christo_campiglia@hotmail.com>
2020-10-15 14:05:08 -0300

    Add Avatar Component.

---
+++ src/components/s-avatar/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="avatar-area">
+    <div class="avatar">
+      <div class="text">{{ nameAsInitials }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+require('../../styles/avatar/main.scss');
+
+export default {
+  name: 'S-Avatar',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    nameAsInitials() {
+      const splittedName = this.name.split(' ');
+      let initials = this.getInitialFromString(splittedName[0]);
+
+      if (splittedName.length > 1) {
+        const lastSplitted = splittedName.pop();
+        initials += this.getInitialFromString(lastSplitted);
+      }
+
+      return initials.toUpperCase();
+    },
+  },
+  methods: {
+    getInitialFromString(string) {
+      return string.substring(0, 1);
+    },
+  },
+};
+</script>
---
+++ src/styles/avatar/README.md
@@ -0,0 +1,7 @@
+# Avatar
+An avatar is a circle used (until now) to show initials, like in a user profile.
+
+In the future it will support:
+- Image
+- Different sizes
+- Different background color
